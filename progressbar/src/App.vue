941a63da5b28f01fb24f1dda61fd0580866de254
Claude Ribeiro <caepatriot@gmail.com>
2023-10-18 13:18:03 +0200

    init

---
+++ progressbar/src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <Progressbar :steps="steps" />
+</template>
+
+<script>
+import Progressbar from './components/Progressbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Progressbar
+},
+  data() {
+    return {
+      steps: [
+        {
+          title: "step 1",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 1",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 2",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 2",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 3",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 3",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 4",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 4",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 5",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 5",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 6",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 6",
+            done: true,
+            has: true
+          }
+        },
+        {
+          title: "step 7",
+          date: new Date(),
+          duration: 10,
+          checkpoint: {
+            title: "checkpoint 7",
+            done: true,
+            has: true
+          }
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ progressbar/src/components/Progressbar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="progressbar">
+
+    <div class="progression"></div>
+
+    <div class="step" v-for="step in steps" :key="step.title">
+      <div v-if="step.checkpoint.has" class="checkpoint"></div>
+      <span class="step-progress-indicator">{{ step.title }}</span>
+    </div>
+
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.progressbar {
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  max-height: 50px;
+  height: 20px;
+  min-height: 10px;
+  width: 50%;
+}
+
+.progression {
+  position: absolute;
+  background: greenyellow;
+  height: 100%;
+  width: 75%;
+  left: 0;
+  /* z-index: 5; */
+}
+
+.step {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid grey;
+  /* background: green; */
+  width: 100%;
+  height: 100%;
+}
+
+.step-progress-indicator {}
+
+.checkpoint {
+  position: absolute;
+  border: 2px solid #fff;
+  background: green;
+  height: 130%;
+  /* width: 200%; */
+  border-radius: 100%;
+  left: 0;
+  transform: translateX(-51%);
+  aspect-ratio: 1/1;
+  z-index: 10;
+}
+</style>
+
+
+<script>
+export default {
+  name: 'Progress-bar',
+  props: ['steps'],
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+    // this.steps = props.steps;
+  },
+}
+</script>
