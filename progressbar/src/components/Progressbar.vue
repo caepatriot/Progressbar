070801510641b79f9fea03f021a82e5a77075c4b
Claude Ribeiro <caepatriot@gmail.com>
2023-10-21 19:32:25 +0200

    Added vertical progressbar

--- progressbar/src/components/Progressbar.vue
+++ progressbar/src/components/Progressbar.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="progressbar" :class="{ vertical: vertical }">
-    <div class="step" v-for="step in steps" :key="step.title" :style="{ 'width': isLongDuration(step) ? '100%' : '0%' }">
-      <div class="progression" :style="{ width: stepProgressWidth(step) + '%' }"></div>
-      <div v-if="step.checkpoint.has" class="checkpoint"></div>
+  <div class="progressbar" :class="isVerticalClass">
+    <div class="step" :class="isVerticalClass" v-for="step in steps" :key="step.title"
+      :style="{ 'width': isLongDuration(step) ? '100%' : '0%' }">
+      <div v-if="vertical" class="progression" :class="isVerticalClass" :style="{ height: stepProgressWidth(step) + '%' }"></div>
+      <div v-else class="progression" :class="isVerticalClass" :style="{ width: stepProgressWidth(step) + '%' }"></div>
+      <div v-if="step.checkpoint.has" class="checkpoint" :class="isVerticalClass"></div>
       <span class="step-progress-indicator">{{
         stepProgressWidth(step) + "%"
       }}</span>
@@ -15,27 +17,22 @@
 .progressbar {
   position: relative;
   display: flex;
-  flex-direction: row;
   justify-content: center;
   align-items: center;
+}
+
+.progressbar.vertical {
+  flex-direction: column;
+  height: 100%;
+  width: 20px;
+}
+
+.progressbar.horizontal {
+  flex-direction: row;
   max-height: 50px;
   height: 20px;
   min-height: 10px;
   width: 50%;
-}
-
-
-// .vertical {
-//   flex-direction: column;
-//   height: 100%;
-//   width: 20px;
-// }
-
-.progression {
-  position: absolute;
-  background: greenyellow;
-  height: 100%;
-  left: 0;
 }
 
 .step {
@@ -45,15 +42,31 @@
   align-items: center;
   border: 2px solid #fff;
   background: #fff;
+}
+
+.step.vertical {
   width: 100%;
   height: 100%;
 }
 
-.step:first-child {
+.step.horizontal {
+  width: 100%;
+  height: 100%;
+}
+
+.step.vertical:first-child {
+  border-radius: 50px 50px 0 0;
+}
+
+.step.vertical:last-child {
+  border-radius: 0 0 50px 50px;
+}
+
+.step.horizontal:first-child {
   border-radius: 50px 0 0 50px;
 }
 
-.step:last-child {
+.step.horizontal:last-child {
   border-radius: 0 50px 50px 0;
 }
 
@@ -61,31 +74,62 @@
   position: absolute;
 }
 
+.step.vertical:nth-child(even) > .step-progress-indicator{
+  transform: translateX(100%);
+}
+
+.step.vertical:nth-child(odd) > .step-progress-indicator{
+  transform: translateX(-100%);
+}
+
 .checkpoint {
   position: absolute;
   border: 2px solid #fff;
   background: green;
-  height: 130%;
   border-radius: 100%;
-  left: 0;
-  transform: translateX(-51%);
   aspect-ratio: 1/1;
   z-index: 10;
 }
 
-.checkpoint-done {
-  /* background-image: url("../../assets/icons/check.png"); */
+.checkpoint.vertical {
+  width: 130%;
+  top: 0;
+  transform: translateY(-51%);
 }
 
-.checkpoint-not-done {}
+.checkpoint.horizontal {
+  height: 130%;
+  left: 0;
+  transform: translateX(-51%);
+}
+
+.progression {
+  position: absolute;
+  background: greenyellow;
+}
+
+.progression.vertical {
+  top: 0;
+  width: 100%;
+}
+
+.progression.horizontal {
+  left: 0;
+  height: 100%;
+}
 </style>
 
 
 <script>
 export default {
   name: "Progress-bar",
-  props: ["steps", "vertical"],
+  // props: ["steps", "vertical"],
+  props: {
+    steps: Object,
+    vertical: Boolean
+  },
   setup() {
+    // console.log(props);
   },
   data() {
     return {
@@ -97,7 +141,9 @@ export default {
   },
 
   computed: {
-
+    isVerticalClass: function () {
+      return this.vertical ? 'vertical' : 'horizontal';
+    }
   },
   methods: {
     stepProgressWidth(step) {
